799ac1ccc050986cbd16025e7d5b6098075a4bd7
blendy22 <blendd.n@gmail.com>
2023-10-12 18:11:10 +0200

    Add simple ui

--- index.js
+++ index.js
@@ -25,13 +25,70 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt("Choose between rock, paper and scissors: ").toLowerCase(); 
+    const container = document.querySelector(".container");
+    const content = document.createElement("div");
+    let yourScore = 0;
+    let pcScore = 0;
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    const h3 = document.createElement("h3");
+    const resetButton = document.createElement("button");
+
+
+    function output(playerSelection) {
         const computerSelection = getComputerChoice();
-        console.log("YOU: " + playerSelection);
-        console.log("PC: " + computerSelection);
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(playerSelection, computerSelection);  
+        p1.textContent = "YOU: " + playerSelection;
+        p2.textContent = "PC: " + computerSelection;
+        let round = playRound(playerSelection, computerSelection);
+        if (round === "YOU WIN!") {
+            yourScore++;
+        } else if (round === "YOU LOSE!") {
+            pcScore++;
+        }
+        if (yourScore === 5) {
+            p3.textContent = "YOU WON!"; 
+            resetButton.textContent = "Restart";
+            resetButton.addEventListener('click', () => {
+                location.reload();
+            });
+            content.appendChild(resetButton); 
+        } else if (pcScore === 5) {
+            p3.textContent = "YOU LOST :(";
+            resetButton.textContent = "Restart";
+            resetButton.addEventListener('click', () => {
+                location.reload();
+            }); 
+            content.appendChild(resetButton); 
+        }
+        h3.textContent = `LEADERBOARD: YOU: ${yourScore} - PC: ${pcScore}`;
+        
+        content.appendChild(p1);
+        content.appendChild(p2);
+        content.appendChild(p3);
+        content.appendChild(h3);
+        container.appendChild(content);
     }
+    p1.remove();
+    p2.remove();
+    h3.remove();
+    
+    const rock = document.querySelector("#btn1");
+    const paper = document.querySelector("#btn2");   
+    const scissors = document.querySelector("#btn3");
+    
+    
+    btn1.addEventListener('click', () => {
+        output("rock");
+        
+    });
+    btn2.addEventListener('click', () => {
+        output("paper");
+    }); 
+    btn3.addEventListener('click', () => {
+        output("scissors");
+    });
 }
 
 game();
